ruby/shellwords

commit 877c04257be31a27495ac224d2c3a890e9690621

* lib/shellwords.rb: Turn on frozen-string-literal after fixing   shellsplit.
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53066 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

lib/shellwords.rb
@@ -1,4 +1,4 @@
-# frozen-string-literal: false
+# frozen-string-literal: true
 ##
 # == Manipulates strings like the UNIX Bourne shell
 #
@@ -70,14 +70,14 @@ module Shellwords
   #   argv #=> ["here", "are", "two words"]
   def shellsplit(line)
     words = []
-    field = ''
+    field = String.new
     line.scan(/\G\s*(?>([^\s\\\'\"]+)|'([^\']*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|\z)?/m) do
       |word, sq, dq, esc, garbage, sep|
       raise ArgumentError, "Unmatched double quote: #{line.inspect}" if garbage
       field << (word || sq || (dq || esc).gsub(/\\(.)/, '\\1'))
       if sep
         words << field
-        field = ''
+        field = String.new
       end
     end
     words

commit 1e5b9fa2439f05228bfcaa96293eaead79a11e7c

lib/shellwords.rb: do not change API with frozen-string-literal
This fixes a bug introduced in r53066 when attempting to
shellescape an empty string.

* lib/shellwords.rb (shellescape): duplicate frozen literal
* test/test_shellwords.rb (test_stringification): new test

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53068 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

lib/shellwords.rb
@@ -125,7 +125,7 @@ module Shellwords
     str = str.to_s
 
     # An empty argument will be skipped, so return empty quotes.
-    return "''" if str.empty?
+    return "''".dup if str.empty?
 
     str = str.dup
 

test/test_shellwords.rb
@@ -48,8 +48,17 @@ class TestShellwords < Test::Unit::TestCase
   end
 
   def test_stringification
-    assert_equal "3", shellescape(3)
-    assert_equal "ps -p #{$$}", ['ps', '-p', $$].shelljoin
+    three = shellescape(3)
+    assert_equal '3', three
+    assert_not_predicate three, :frozen?
+
+    empty = shellescape('')
+    assert_equal "''", empty
+    assert_not_predicate empty, :frozen?
+
+    joined = ['ps', '-p', $$].shelljoin
+    assert_equal "ps -p #{$$}", joined
+    assert_not_predicate joined, :frozen?
   end
 
   def test_multibyte_characters